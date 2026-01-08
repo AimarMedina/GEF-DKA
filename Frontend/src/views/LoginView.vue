dfb1f5e07ef050265b4357941d00de2aa2a44804
Aimar Medina <142GA03@arriagainfo.local>
2026-01-08 08:46:54 +0100

    dasdas

---
+++ Frontend/src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import loginForm from '@/components/loginForm.vue'
+</script>
+
+<template>
+  <div class="s">
+    <loginForm />
+  </div>
+</template>
+
+<style scoped>
+.container{
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+</style>
