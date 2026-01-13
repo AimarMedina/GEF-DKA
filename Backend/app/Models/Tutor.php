3e953e512cbbb807fbeb3503e4e886c1c6840929
Ibai <ibai.lopez.resta@ikasle.egibide.org>
2026-01-13 10:13:02 +0100

    isaurYG

--- Backend/app/Models/Tutor.php
+++ Backend/app/Models/Tutor.php
@@ -8,10 +8,6 @@ class Tutor extends Model
 {
     protected $table = "tutor";
     protected $primary="ID_Usuario";
-<<<<<<< HEAD
-=======
-    public $incrementing = false;
->>>>>>> 2147e41d40a0579ca9c927e7e412719f4a65c7e5
     protected function tutor(){
         return $this->belongsTo(User::class,"ID_Usuario","ID");
     }
