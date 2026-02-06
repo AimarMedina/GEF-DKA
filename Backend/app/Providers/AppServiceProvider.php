46686d46a8749b1c0a9963653ca0b620915dc7ab
Iker Hernaez <80524650+ikeerhg@users.noreply.github.com>
2026-02-06 14:04:39 +0100

    quitamos lo del gate

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -19,24 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Gate para verificar si es admin
-        Gate::define('es-admin', function (User $user) {
-            return $user->tipo === 'admin';
-        });
 
-        // Gate para verificar si es tutor
-        Gate::define('es-tutor', function (User $user) {
-            return $user->tipo === 'tutor';
-        });
-
-        // Gate para verificar si es instructor
-        Gate::define('es-instructor', function (User $user) {
-            return $user->tipo === 'instructor';
-        });
-
-        // Gate para verificar si es alumno
-        Gate::define('es-alumno', function (User $user) {
-            return $user->tipo === 'alumno';
-        });
     }
 }
